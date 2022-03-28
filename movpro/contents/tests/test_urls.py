605709f341c5f9477f89e9f9b9b0557e3468c734
salimildere <salim.ildere@akinon.com>
2022-03-28 22:29:36 +0300

    Add content url test cases.

---
+++ movpro/contents/tests/test_urls.py
@@ -0,0 +1,31 @@
+from django.test import TestCase, override_settings
+from django.urls import resolve, reverse
+
+
+class TestContentURLAPIViews(TestCase):
+    """
+        run:
+        python manage.py test movpro.contents.tests.test_urls.TestContentURLAPIViews --keepdb
+
+    """
+
+    def test_content_basic_urls(self):
+
+        url_name_map = (
+            ("/api/v1/content/", "contents:content"),
+            ("/api/v1/content/create_mock/", "contents:create_mock_content"),
+        )
+
+        for url, name in url_name_map:
+            self.assertEqual(reverse(name), url)
+            self.assertEqual(resolve(url).view_name, name)
+
+    def test_content_urls_with_parameters(self):
+        url_name_map = (
+            ("/api/v1/content/", "contents:content_detail", "pk"),
+        )
+        for url, name, param in url_name_map:
+            kwargs = {param: "123"}
+            url = url + kwargs[param] + "/"
+            self.assertEqual(reverse(name, kwargs=kwargs), url)
+            self.assertEqual(resolve(url).view_name, name)
