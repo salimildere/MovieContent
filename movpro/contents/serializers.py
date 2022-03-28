5540f0a165e9601fa1203596bb6c4b7250dd3dc7
salimildere <salim.ildere@akinon.com>
2022-03-28 05:36:33 +0300

    Create ContentDetailSerializer. Create ContentBaseSerializer with inherit.

--- movpro/contents/serializers.py
+++ movpro/contents/serializers.py
@@ -2,14 +2,20 @@ from rest_framework import serializers
 
 from movpro.contents.models import Content
 
+class ContentBaseSerializer(serializers.ModelSerializer):
+    director = serializers.StringRelatedField()
+    genre = serializers.StringRelatedField(many=True)
+    content_rating = serializers.StringRelatedField()
 
-class ContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Content
+        fields = '__all__'
+
+class ContentSerializer(ContentBaseSerializer):
     class Meta:
         model = Content
         fields = ('id', 'title', 'imdb_rating', 'made_year', 'image', 'director', 'genre', 'content_rating')
 
 
-class ContentDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Content
-        fields = '__all__'
+class ContentDetailSerializer(ContentBaseSerializer):
+    actress = serializers.StringRelatedField(many=True)
