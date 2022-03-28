897611292c193b836cd68c4342c595a0003805a9
salimildere <salim.ildere@akinon.com>
2022-03-28 05:37:32 +0300

    Create ContentCreateMockViewSet for test and local env.

--- movpro/contents/urls.py
+++ movpro/contents/urls.py
@@ -1,7 +1,12 @@
+from django.conf import settings
 from django.urls import path
-from movpro.contents.views import ContentViewSet, ContentDetailViewSet
+from movpro.contents.views import ContentViewSet, ContentDetailViewSet, ContentCreateMockViewSet
 
 urlpatterns = [
-    path("/", ContentViewSet.as_view(), name="content"),
-    path("/<int:pk>", ContentDetailViewSet.as_view(), name="content_detail"),
+    path("", ContentViewSet.as_view(), name="content"),
+    path("<int:pk>/", ContentDetailViewSet.as_view(), name="content_detail"),
 ]
+if settings.IS_TEST_ENV:
+    urlpatterns += [
+        path("create_mock/", ContentCreateMockViewSet.as_view(), name="create_mock_content"),
+    ]
--- movpro/contents/views.py
+++ movpro/contents/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
 
 from movpro.contents.models import Content
 from movpro.contents.serializers import ContentSerializer, ContentDetailSerializer
+from movpro.contents.tests.factories import ContentFactory
 
 
 class ContentViewSet(generics.ListAPIView):
@@ -13,3 +15,12 @@ class ContentViewSet(generics.ListAPIView):
 class ContentDetailViewSet(generics.RetrieveUpdateDestroyAPIView):
     queryset = Content.objects.all()
     serializer_class = ContentDetailSerializer
+
+
+class ContentCreateMockViewSet(GenericAPIView):
+    serializer_class = ContentDetailSerializer
+
+    def post(self, *args, **kwargs):
+        content = ContentFactory()
+        serializer = ContentDetailSerializer(content)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
